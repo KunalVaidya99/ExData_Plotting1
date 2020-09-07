285f19846953107c53f2b19ac98b2f55fac18465
KunalVaidya99 <69197443+KunalVaidya99@users.noreply.github.com>
2020-09-07 22:47:37 +0530

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -5,5 +5,5 @@ powerdata <- powerdata[2:dim(powerdata)[1],]
 
 sub <- powerdata[powerdata$Date=="2/2/2007"|powerdata$Date=="1/2/2007",]
 png(file="plot1.png",height=480,width=480)
-hist(as.numeric(sub$Global_active_power),main="Global Active Power",col="orangered",xlab="Global Active Power (kilowatts")
+hist(as.numeric(sub$Global_active_power),main="Global Active Power",col="orangered",xlab="Global Active Power (kilowatts)")
 dev.off()
